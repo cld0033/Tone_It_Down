ed9fe428ada6b3adc691f86369f93893e89f7906
TessHutchinson <165493472+TessHutchinson@users.noreply.github.com>
2024-11-01 23:32:04 -0700

    Adding frontend component :)

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">Desktop Extension Layout</div>
+      <div className="App-content">
+        <button>Desktop Button</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
